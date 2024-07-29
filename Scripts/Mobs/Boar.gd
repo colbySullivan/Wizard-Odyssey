1866befb52ab59b928d7d4c8019cf990cba6135c
Colby Sullivan <colbywalkersullivan@gmail.com>
2024-07-29 15:36:59 -0500

    Interest timer
    Boar loses interest after 1 second out of range

--- Scripts/Mobs/Boar.gd
+++ Scripts/Mobs/Boar.gd
@@ -5,6 +5,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+@onready var interest_timer = $InterestTimer
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -27,3 +28,12 @@ func _on_hit_box_body_entered(body):
 		# Need to make this more efficient
 		await sprite.animation_looped
 		queue_free()
+
+
+func _on_attack_area_body_exited(body):
+	if(body.name == "CharacterBody2D"):
+		interest_timer.start()
+
+
+func _on_timer_timeout():
+	fsm.change_state("idle")
